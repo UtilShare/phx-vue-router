9cbf1c7fec081fcbbcea74b863bd7aeb50d14d0c
Ben Muschol <benmuschol@gmail.com>
2017-11-03 10:23:00 -0400

    Get vue components working (#1)

---
+++ assets/js/components/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <h1>Hello, from Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted() {
+    console.log('Hello, world!');
+  }
+}
+</script>
